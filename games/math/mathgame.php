c999c231b045914e580472936f60d71ed137eeb8
drotondi93 <drotondi93@gmail.com>
2014-12-07 16:17:46 -0500

    deleted unnessisary files

--- games/math/mathgame.php
+++ games/math/mathgame.php
@@ -18,25 +18,5 @@ class mathgame{
             }
         }
     }
-    
-    ////Generate argument variables
-    //public $arg1;
-    //public $arg2;
-    ////Generate opperator variables
-    //public $oppindex=0;
-    //public $opperators=array('+','-','*','/');
-    ////Generate answer variable
-    //public $answer=0;
-    ////Generate gametype and modifer variables
-    //public $gametype='';
-    //public $mod='';
-    ////Generate score variables
-    //public $right=0;
-    //public $wrong=0;
-    ////Generate problem set variable
-    //public $problemset=25;
-    ////Generate useranswer variable
-    //public $useranswer="";
-
 }
 ?>
